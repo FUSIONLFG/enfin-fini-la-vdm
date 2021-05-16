24b56baec56dd8e0d5319f97586b2a5c6ea3e3cb
FUSIONLFG <84091839+FUSIONLFG@users.noreply.github.com>
2021-05-16 09:33:07 +0200

    Add files via upload

---
+++ banned.js
@@ -0,0 +1,4 @@
+module.exports = {
+    run: message => message.channel.send('Dans ce salon, une personne va prendre un ban !'),
+    name: 'banned'
+}
---
+++ index.js
@@ -0,0 +1,57 @@
+const Discord = require('discord.js'),
+    client = new Discord.Client({
+		fetchAllMembers: true
+	}),
+    config = require('./config.json'),
+    fs = require('fs')
+
+client.login(config.token)
+client.commands = new Discord.Collection()
+
+fs.readdir('./commands', (err, files) => {
+    if (err) throw err
+    files.forEach(file => {
+        if (!file.endsWith('.js')) return
+        const command = require(`./commands/${file}`)
+        client.commands.set(command.name, command)
+    })
+})
+
+client.on('message', message => {
+    if (message.type !== 'DEFAULT' || message.author.bot) return
+
+    const args = message.content.trim().split(/ +/g)
+    const commandName = args.shift().toLowerCase()
+    if (!commandName.startsWith(config.prefix)) return
+    const command = client.commands.get(commandName.slice(config.prefix.length))
+    if (!command) return
+    command.run(message, args, client)
+})
+
+const got = require('got');
+
+client.on('message', message => {
+if (message.content === "+meme") {
+    const embed = new Discord.MessageEmbed()
+    got('https://www.reddit.com/r/FrenchMemes/random/.json').then(response => {
+        let content = JSON.parse(response.body);
+        let permalink = content[0].data.children[0].data.permalink;
+        let memeUrl = `https://reddit.com${permalink}`;
+        let memeImage = content[0].data.children[0].data.url;
+        let memeTitle = content[0].data.children[0].data.title;
+        let memeUpvotes = content[0].data.children[0].data.ups;
+        let memeDownvotes = content[0].data.children[0].data.downs;
+        let memeNumComments = content[0].data.children[0].data.num_comments;
+        embed.setTitle(`${memeTitle}`)
+        embed.setURL(`${memeUrl}`)
+        embed.setImage(memeImage)
+        embed.setColor('RANDOM')
+        embed.setFooter(`👍 ${memeUpvotes} 👎 ${memeDownvotes} 💬 ${memeNumComments}`)
+        message.channel.send(embed);
+    })
+}
+})
+
+
+client.login(process.env.token);
+
